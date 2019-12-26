25afd7fcaf1a08697b1fda1e5427eb0b5161793e
Tal Mashiah <talmashiah@gmail.com>
2019-12-26 13:05:15 +0200

    notes imgs css height

--- notes/cmps/NoteImg.jsx
+++ notes/cmps/NoteImg.jsx
@@ -8,7 +8,7 @@ export default class NoteImg extends React.Component {
     render() {
         const { props } = this;
         return <div className="note-container" onClick={this.handleClick}>
-                <img className="note-img" src={props.note.info.url} alt="note image" height="175" />
+                <img className="note-img" src={props.note.info.url} alt="note image"/>
                 <div>{props.note.info.title}</div>
                 <div>{props.note.info.txt}</div>
             </div>
--- notes/cmps/NoteImgModal.jsx
+++ notes/cmps/NoteImgModal.jsx
@@ -14,7 +14,7 @@ export default class NoteImgModal extends React.Component {
         return <div className="modal" >
             <div className="inner-container">
                 <span className="close-Modal" onClick={() => this.handleCloseModal(true)}><i className="fas fa-window-close"></i></span>
-                <img className="note-img" src={props.note.info.url} alt="note image" height="400" />
+                <img className="note-img" src={props.note.info.url} alt="note image"/>
                 <div>{props.note.info.title}</div>
                 <div>{props.note.info.txt}</div>
             </div>
