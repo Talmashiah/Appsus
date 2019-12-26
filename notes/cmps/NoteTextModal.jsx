d72ded2694861db4efba564fd8ad63620cf6014c
Tal Mashiah <talmashiah@gmail.com>
2019-12-26 12:11:54 +0200

    close Modal

--- notes/cmps/NoteTextModal.jsx
+++ notes/cmps/NoteTextModal.jsx
@@ -2,7 +2,7 @@
 export default class NoteTextModal extends React.Component {
 
     handleCloseModal = (isClose) => {
-        if(isClose){
+        if (isClose) {
             this.props.onCloseModal()
         } else {
             return;
@@ -11,8 +11,9 @@ export default class NoteTextModal extends React.Component {
 
     render() {
         const { props } = this;
-        return <div className="modal" onClick={()=>this.handleCloseModal(true)}>
-            <div className="inner-container" onClick={()=>this.handleCloseModal(false)}>
+        return <div className="modal" >
+            <div className="inner-container">
+                <span className="close-Modal" onClick={() => this.handleCloseModal(true)}><i className="fas fa-window-close"></i></span>
                 <div className="modal-title">{props.note.info.title}</div>
                 <div>{props.note.info.txt}</div>
             </div>
