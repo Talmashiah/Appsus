1b93ecf8bc4e471f48fb719ea8b20a2c45258eca
shmuelelkis <shmuel7e@gmail.com>
2019-12-28 17:51:43 +0200

    BOOYAHHH

--- emails/cmps/EmailFilter.jsx
+++ emails/cmps/EmailFilter.jsx
@@ -3,6 +3,7 @@ export default class EmailFilter extends React.Component {
         filterBy: {
             name: '',
         },
+        bg: '',
     }
     changeInput = (ev) => {
         const field = ev.target.name;
@@ -20,11 +21,24 @@ export default class EmailFilter extends React.Component {
         this.props.onSetFilter(ev.target.value);
     }
 
+    onChangeBg = (ev) => {
+        const value = ev.target.value;
+        this.setState(({ bg: value }), function () {
+            console.log(this.state.bg);
+        })
+    }
+
+    changeBg = () => {
+        document.body.style.backgroundImage = `url(${this.state.bg})`;
+    }
+
     render() {
         return <div className="email-list-header">
             <span><i class="fas fa-search"></i></span>
             <div className={'search-input-container'}><input className="search-input" type="text" placeholder="Search Mail" value={this.state.filterBy.name}
                 onChange={this.changeInput} name="name"></input></div>
+            <div className={'search-bg-container'}><input className={'search-bg-input'} type="text" placeholder="Set Background URL"
+                value={this.state.bg} onChange={this.onChangeBg}></input><button className={'bg-btn'} onClick={this.changeBg}>Change</button></div>
             <div className={'dropdown'}>
                 <button className={'dropbtn'}>Search By</button>
                 <div className={'dropdown-content'}>
